8863a1b87de8e01a1e674cc0bf54d4bf27678e44
Tony Arnold <tony@thecocoabots.com>
2016-11-24 14:13:53 +1100

    Make the viewModel property computed

--- File Templates/CocoaBots/View Model and Controller.xctemplate/NSViewControllerNSObjectSwift/___FILEBASENAME___.swift
+++ File Templates/CocoaBots/View Model and Controller.xctemplate/NSViewControllerNSObjectSwift/___FILEBASENAME___.swift
@@ -5,12 +5,14 @@ import Cocoa
 
 final class ___VARIABLE_viewControllerName:identifier___: ___VARIABLE_viewControllerSubclass___ {
 
-    private var viewModel: ___VARIABLE_viewModelName:identifier___?
-
     override var representedObject: Any? {
         willSet {
             precondition(newValue is ___VARIABLE_viewModelName:identifier___?, "Please pass a view model of the correct type (___VARIABLE_viewModelName:identifier___) to this view controller!")
         }
+    }
+
+    private var viewModel: ___VARIABLE_viewModelName:identifier___? {
+        return self.representedObject as? ___VARIABLE_viewModelName:identifier___
     }
 
 }
