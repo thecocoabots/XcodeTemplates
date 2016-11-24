985788b68e0e691a8434e2f9fa97f74fd1c9f18e
Tony Arnold <tony@thecocoabots.com>
2016-11-24 13:59:28 +1100

    Fix representedObject type to "Any?" in the Swift view controller template

--- File Templates/CocoaBots/View Model and Controller.xctemplate/NSViewControllerNSObjectSwift/___FILEBASENAME___.swift
+++ File Templates/CocoaBots/View Model and Controller.xctemplate/NSViewControllerNSObjectSwift/___FILEBASENAME___.swift
@@ -7,7 +7,7 @@ final class ___VARIABLE_viewControllerName:identifier___: ___VARIABLE_viewContro
 
     private var viewModel: ___VARIABLE_viewModelName:identifier___?
 
-    override var representedObject: AnyObject? {
+    override var representedObject: Any? {
         willSet {
             precondition(newValue is ___VARIABLE_viewModelName:identifier___?, "Please pass a view model of the correct type (___VARIABLE_viewModelName:identifier___) to this view controller!")
         }
